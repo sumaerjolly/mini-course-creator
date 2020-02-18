875ed25210fd498622313b9b9b16ef50bffda479
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-18 13:37:52 +0530

    set up things by rendering home component

---
+++ app/javascript/components/home/Home.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      course_modules: [
+        {
+          id: 1,
+          title: '1. Setting up a new ROR app with React',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          active: false
+        },
+        {
+          id: 2,
+          title: '2. Adding react to an existing rails app',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          active: false
+        },
+        {
+          id: 3,
+          title: '3. Building a hello world app',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          active: false
+        },
+        {
+          id: 4,
+          title: '4. Adding React Router Dom to your App',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          active: false
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Testing</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -4,22 +4,12 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.css';
-
-const Hello = props => <div>Hello {props.name}!</div>;
-
-Hello.defaultProps = {
-  name: 'David'
-};
-
-Hello.propTypes = {
-  name: PropTypes.string
-};
+import Home from '../components/home/Home';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Home />,
     document.body.appendChild(document.createElement('div'))
   );
 });
