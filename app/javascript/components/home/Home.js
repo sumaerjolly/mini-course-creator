6f383c1f83f7bbd679c818a77b7968fd2a9b9405
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-18 18:03:05 +0530

    connected db to react and rendered using db

--- app/javascript/components/home/Home.js
+++ app/javascript/components/home/Home.js
@@ -1,42 +1,37 @@
 import React, { Component } from 'react';
 import Jumbotron from './Jumbotron';
 import Table from './Table/Table';
+import axios from 'axios';
 
 export class Home extends Component {
   constructor() {
     super();
     this.state = {
-      course_modules: [
-        {
-          id: 1,
-          title: '1. Setting up a new ROR app with React',
-          description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-          active: false
-        },
-        {
-          id: 2,
-          title: '2. Adding react to an existing rails app',
-          description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-          active: false
-        },
-        {
-          id: 3,
-          title: '3. Building a hello world app',
-          description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-          active: false
-        },
-        {
-          id: 4,
-          title: '4. Adding React Router Dom to your App',
-          description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-          active: false
-        }
-      ]
+      course_modules: []
     };
+  }
+
+  componentDidMount() {
+    axios
+      .get('/episodes.json')
+      .then(data => {
+        debugger;
+        let result = [];
+        data.data.data.map(data => {
+          result.push({
+            id: data.id,
+            title: data.title,
+            description: data.description,
+            active: false
+          });
+          this.setState({
+            course_modules: result
+          });
+        });
+      })
+      .catch(data => {
+        debugger;
+      });
   }
 
   handleClickVideo(item, e) {
