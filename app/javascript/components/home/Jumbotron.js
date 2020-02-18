7c04dc0d638f4bdf52d3d4234bc2d5bb52e63140
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-18 14:07:27 +0530

    header and subheader styling added using styled components

--- app/javascript/components/home/Jumbotron.js
+++ app/javascript/components/home/Jumbotron.js
@@ -4,8 +4,20 @@ import styled from 'styled-components';
 const Section = styled.section`
   background-color: #d74234;
   min-height: 550px;
-  padding: 250px 0;
+  padding: 100px 0;
   color: white;
+`;
+
+const Header = styled.h1`
+  color: white;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 52px;
+`;
+
+const Subhead = styled.p`
+  font-size: 18pxSubhead
+  font-weight: 500;
 `;
 
 function Jumbotron() {
@@ -16,8 +28,8 @@ function Jumbotron() {
           <div className="row">
             <div className="col col-sm col-md-5">
               <div className="pt-4 mt-4">
-                <h1>React For Rails Developers</h1>
-                <p>SuperCharge your Ruby on Rails App with React.js</p>
+                <Header>React For Rails Developers</Header>
+                <Subhead>SuperCharge your Ruby on Rails App with React.js</Subhead>
               </div>
             </div>
             <div className="col col-sm col-md-7">
