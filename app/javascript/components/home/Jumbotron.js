38bc9e6dcedbbadbc11b76dcae953a35c9d147cb
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-18 14:39:24 +0530

    jumbotron component completed

--- app/javascript/components/home/Jumbotron.js
+++ app/javascript/components/home/Jumbotron.js
@@ -20,6 +20,20 @@ const Subhead = styled.p`
   font-weight: 500;
 `;
 
+const Button = styled.a`
+  display: inline-block;
+  text-decoration: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 0;
+  background: white;
+  color: #333 !important;
+  padding: 10px 20px;
+  font-size: 18px;
+  box-shadow: 0px 0px 0px 3px #473228, -6px 6px #ef5f17,
+    -6px 6px 0px 3px #473229;
+`;
+
 function Jumbotron() {
   return (
     <div>
@@ -29,7 +43,12 @@ function Jumbotron() {
             <div className="col col-sm col-md-5">
               <div className="pt-4 mt-4">
                 <Header>React For Rails Developers</Header>
-                <Subhead>SuperCharge your Ruby on Rails App with React.js</Subhead>
+                <Subhead>
+                  SuperCharge your Ruby on Rails App with React.js
+                </Subhead>
+                <div className="cta-wrapper">
+                  <Button className="btn fancy-btn">Get Started</Button>
+                </div>
               </div>
             </div>
             <div className="col col-sm col-md-7">
